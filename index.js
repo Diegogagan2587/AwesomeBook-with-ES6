a980cf2298d0a4f1edcb9a3e03ac367ae3e7712d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-05 15:00:30 -0600

    remove unecesary code from index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import { updateLocalStorage } from "./modules/local_storage.js";
-import { display } from "./modules/diplay_books.js";
-import { showClickedSection } from "./modules/switch_sections.js";
-import { addRemoveListeners } from "./modules/remove_listeners.js";
-import { addBook } from "./modules/add_book.js";
 import { setListenersToNavigationBar } from "./modules/navigation_bar.js";
 import { Books } from "./modules/books_constructor.js";
 
