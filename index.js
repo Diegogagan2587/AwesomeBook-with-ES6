9bb1e4451c61705c838337f91b222209f16f6d42
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-05 14:32:27 -0600

    refactor addBook function to a module, import add_book.js module to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { updateLocalStorage } from "./modules/local_storage.js";
 import { display } from "./modules/diplay_books.js";
 import { showClickedSection } from "./modules/switch_sections.js";
 import { addRemoveListeners } from "./modules/remove_listeners.js";
+import { addBook } from "./modules/add_book.js";
 
 /* --Navigation Bar--*/
 
@@ -18,20 +19,11 @@ class Books {
     constructor() {
         this.bookList = JSON.parse(localStorage.getItem('bookList')) || [];
         const addBookButton = document.querySelector('#addBook');
-        addBookButton.addEventListener('click', this.addBook.bind(this));
+        addBookButton.addEventListener('click', addBook.bind(this));
         if (this.bookList !== null) {
             display(this);
             addRemoveListeners(this);
         }
-    }
-
-    addBook() {
-        const addtitle = document.querySelector('#title').value;
-        const addauthor = document.querySelector('#author').value;
-        this.bookList.push({ title: addtitle, author: addauthor });
-        display(this);
-        addRemoveListeners(this);
-        updateLocalStorage(this);
     }
 }
 
---
+++ modules/add_book.js
@@ -0,0 +1,14 @@
+import { display } from "./diplay_books.js";
+import { addRemoveListeners } from "./remove_listeners.js";
+import { updateLocalStorage } from './local_storage.js';
+
+function addBook() {
+    const addtitle = document.querySelector('#title').value;
+    const addauthor = document.querySelector('#author').value;
+    this.bookList.push({ title: addtitle, author: addauthor });
+    display(this);
+    addRemoveListeners(this);
+    updateLocalStorage(this);
+}
+
+export { addBook };
