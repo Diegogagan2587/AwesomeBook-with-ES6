0d468834f1642bab182c3e4323c58340cd0c0653
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-13 23:26:44 -0600

    remove unecesary spaces

--- modules/diplay_books.js
+++ modules/diplay_books.js
@@ -3,7 +3,7 @@ function display(thisClass) {
   tbody.innerHTML = '';
   let index = 0;
   thisClass.bookList.forEach((element) => {
-    const currentRow = document.createElement('tr');
+    const currentRow = document.createElement('tr'); 
     const currentTitle = element.title;
     const currentAuthor = element.author;
     currentRow.innerHTML = `<div>"${currentTitle}"&nbsp;&nbsp<span>by</span>&nbsp;&nbsp ${currentAuthor}</div> <div><button class='removeButton' value="${index}">Remove</button></div> `;
